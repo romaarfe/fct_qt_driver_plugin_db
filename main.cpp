589008ca11ea272f826057db505059d014cc296a
Rodrigo Fernandes <romaarfe@gmail.com>
2023-08-02 08:40:37 +0100

    Test Plugin

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+
+#include <QCoreApplication>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QSqlError>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // Carregar o plugin do SQLCipherDB
+    QCoreApplication::addLibraryPath("/home/administrator/Qt/6.5.1/gcc_64/plugins/sqldrivers/qsql_ciphersqlite.so"); // Altere para o caminho correto onde o plugin foi construído
+    QSqlDatabase::addDatabase("QSQLCIPHER");
+
+    // Abrir conexão com o banco de dados SQLCipherDB
+    QSqlDatabase db = QSqlDatabase::database();
+    if (db.isOpen()) {
+        qDebug() << "Connected to SQLCipherDB!";
+    } else {
+        qDebug() << "Failed to connect to SQLCipherDB!";
+    }
+
+    // Executar uma consulta
+    QSqlQuery query("SELECT * FROM tabela"); // Substitua pela consulta desejada
+    if (query.exec()) {
+        while (query.next()) {
+            // Ler e processar os resultados da consulta
+        }
+    } else {
+        qDebug() << "Query failed:" << query.lastError().text();
+    }
+
+    // Fechar a conexão com o banco de dados SQLCipherDB
+    db.close();
+
+    return a.exec();
+}
